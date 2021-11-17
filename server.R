44d16d8ba2e1e5cb7456a96504b2ec2cdd964e81
Lorenzo <lorenzokruidenier@hotmail.com>
2021-11-17 16:55:23 +0100

    update main branch

---
+++ server.R
@@ -0,0 +1,15 @@
+server <- function(input, output){
+  
+  points <- eventReactive(input$recalc, {
+    cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
+  }, ignoreNULL = FALSE)
+  
+  output$mymap <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles(providers$Stamen.TonerLite,
+                       options = providerTileOptions(noWrap = TRUE)
+      ) %>%
+      addMarkers(data = points())
+  })
+  
+}
