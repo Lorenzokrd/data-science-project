bce58d7b5bf77977c98c43481047aa1eb3428cbe
Lorenzo <lorenzokruidenier@hotmail.com>
2021-11-22 21:54:51 +0100

    Leaflet map in shiny with choropleth

--- ui.R
+++ ui.R
@@ -1,10 +1,37 @@
 library(leaflet)
 library(shiny)
+library(cbsodataR)
+library(tidyverse)
+library(sf)
+library(leaflet)
+library(ggplot2)
+library(plotly)
+library(jsonlite)
+library(geojsonio)
+library(sp)
+library(rgdal)
+library(dplyr)
+library(shinyjs)
+library(treemapify)
+library(shinythemes)
+library(stringr)
 
 ui <- fluidPage(
+  theme = shinytheme("slate"),
+  useShinyjs(),
+  navbarPage("Crime dashboard",
+    tabPanel("Map",
+      sidebarLayout(
+        sidebarPanel( width = 3,
+          tags$h3("Plot parameters")
+        ),
+        mainPanel( width = 9, 
+          tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
+          leafletOutput("map"),
+        )
+      )
+    )
+  )
   
-  leafletOutput("mymap"),
-  p(),
-  actionButton("recalc", "New points")
   
 )
